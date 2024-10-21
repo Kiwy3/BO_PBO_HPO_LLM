91854a9c1cc454763fe8129ef9768cbfdfe419f8
Kiwy3 <nathan.davouse@gmail.com>
2024-10-21 13:29:37 +0200

    add grades batches as HP

--- Single/optimization_test.py
+++ Single/optimization_test.py
@@ -1,17 +1,19 @@
 import json
 from validate_test import BB_eval
 
-lr_list = [0.001,0.002,0.003]
-lr_list = [1e-2, 1e-3, 1e-3]
+
+lr_list = [1e-2, 1e-4, 1e-5]
 rank =   [16,8,4]
+gb_list = [6,8,16]
 
 HP = {}
 out_list = []
 
-for i in range(3):
+for i in range(len(lr_list)):
     print(f"iteration number {i+1} :")
     HP["learning_rate"] = lr_list[i]
     HP["lora_rank"] = rank[i]
+    HP["grad_batches"] = gb_list[i]
     print(HP)
 
     out = BB_eval(HP)
