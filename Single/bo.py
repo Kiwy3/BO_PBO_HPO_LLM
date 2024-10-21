6df483ffb5b33a4a6fb116245a4313e5e073b4f8
Kiwy3 <nathan.davouse@gmail.com>
2024-10-21 16:43:36 +0200

    comment and adapt to 2D X

--- Single/bo.py
+++ Single/bo.py
@@ -1,17 +1,18 @@
-"""----------------Import librairies ------------------------------"""
-#Standard librairy
+""" --------------Import librairies ------------------------------"""
+# Standard librairy
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-#NN lib
+# NN lib
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.model_selection import train_test_split 
 
-#Bayesian function
+# Bayesian function
 from botorch.models import SingleTaskGP
+from botorch.models.gp_regression_mixed import MixedSingleTaskGP
 from botorch.models.transforms import Normalize, Standardize
 from botorch.fit import fit_gpytorch_mll
 from gpytorch.mlls import ExactMarginalLogLikelihood
@@ -25,57 +26,64 @@ warnings.filterwarnings("ignore")
 
 
 
-
+import torch, math
 HP = {}
-X = torch.tensor([[-1]],dtype=torch.double)
-HP["learning_rate"] = math.exp(X)
+X = torch.tensor([[-1,4]],dtype=torch.double)
+HP["learning_rate"] = math.exp(X[0][0])
+HP["lora_rank"] = round(X[0][1].item())
 val_loss = BB_eval(HP)
 Y = torch.tensor([[val_loss]],dtype=torch.double)
 
 
-"""-------------Bayesian optimisation-------------------"""
-lower_bound,higher_bound  = -10,-1
-bounds = torch.stack([torch.tensor([lower_bound]),torch.tensor([higher_bound])]).to(torch.double)
+""" --------------Bayesian optimisation-------------------"""
+lower_bound = [-10, 2]
+higher_bound = [-1, 32]
+bounds = torch.stack(
+    [torch.tensor(lower_bound),
+     torch.tensor(higher_bound)]
+     ).to(torch.double)
 n_BO = 5
 
-#PLOT
-plt.scatter(X,Y,c="red",alpha=0.5)
-plt.xlim((lower_bound,higher_bound))
-plt.title("Iterative bayesian optimisation")
+# PLOT
+# plt.scatter(X[0],Y,c="red",alpha=0.5)
+# plt.xlim((lower_bound,higher_bound))
+# plt.title("Iterative bayesian optimisation")
 
 
-"""-------------Bayesian iterations-------------------"""
+""" --------------Bayesian iterations-------------------"""
 for i in range(n_BO):
-    #Define the model
-    gp = SingleTaskGP(
+    # Define the model
+    gp = MixedSingleTaskGP(
     train_X=X,
     train_Y=Y,
-    input_transform=Normalize(d=1),
+    cat_dims=[-1],
+    input_transform=Normalize(d=2),
     outcome_transform=Standardize(m=1),
     )
 
-    #Optimize the model
+    # Optimize the model
     mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
     fit_gpytorch_mll(mll)
     logEI = LogExpectedImprovement(model=gp, best_f=Y.max())
     candidate, acq_value = optimize_acqf(
         logEI, bounds=bounds, q=1, num_restarts=5, raw_samples=20,
     )
-
-    #Compte new evaluation
     print(candidate)
-    HP["learning_rate"] = math.exp(candidate)
+    # Compute the new evaluation
+    HP["learning_rate"] = math.exp(candidate[0][0].item())
+    HP["lora_rank"] = round(candidate[0][1].item())
     val_loss = BB_eval(HP)
 
-    #Append new data
-    Y_candidate = torch.tensor(val_loss).reshape_as(candidate)
+    # Append new data
+    Y_candidate = torch.tensor([[val_loss]],dtype=torch.double)
     X = torch.cat((X,candidate))
     Y = torch.cat((Y,Y_candidate))
 
-    #Plot the new point
-    plt.scatter(candidate,val_loss,c="black",alpha=0.8)
-    plt.text(candidate,val_loss+0.02,str(i))
+    # Plot the new point
+    # plt.scatter(candidate,val_loss,c="black",alpha=0.8)
+    # plt.text(candidate,val_loss+0.02,str(i))
 
+# Print the current state of X and Y, and save the plot as "Single_BO.png"
 print(X, Y)
-plt.savefig("Single_BO.png")
+#plt.savefig("Single_BO.png")
 
