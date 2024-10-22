24fd3e42bd46ce3e5f9bbfb22e79b750723a890e
Kiwy3 <nathan.davouse@gmail.com>
2024-10-22 16:01:22 +0200

    first approach using Zeliij for LLM

---
+++ Single/zellij_opt.py
@@ -0,0 +1,60 @@
+from zellij.core.geometry import Direct
+from zellij.strategies import DBA
+from zellij.strategies.tools.tree_search import Potentially_Optimal_Rectangle
+from zellij.strategies.tools.direct_utils import Sigma2, SigmaInf
+
+from zellij.core import ContinuousSearchspace, FloatVar, ArrayVar, Loss
+from zellij.utils.benchmarks import himmelblau
+
+import math
+from bb_llm import BB_eval
+
+def loss_function(x):
+    HP = {
+        "learning_rate": math.exp(x[0]),
+        "lora_rank": x[1],
+        "fast_run" : True
+    }
+    print(dir(x))
+    return BB_eval(HP)
+
+loss = Loss()(loss_function)
+values = ArrayVar(
+                  FloatVar("learning_rate",-10,-1),
+                  FloatVar("lora_rank",2,32)
+                  )
+                  
+
+def Direct_al(
+  values,
+  loss,
+  calls,
+  verbose=True,
+  level=600,
+  error=1e-4,
+  maxdiv=3000,
+  force_convert=False,
+):
+
+  sp = Direct(
+      values,
+      loss,
+      calls,
+      sigma=Sigma2(len(values)),
+  )
+
+  dba = DBA(
+      sp,
+      calls,
+          tree_search=Potentially_Optimal_Rectangle(
+          sp, level, error=error, maxdiv=maxdiv
+      ),
+      verbose=verbose,
+  )
+  dba.run()
+
+  return sp
+
+sp = Direct_al(values, loss, 1000)
+best = (sp.loss.best_point, sp.loss.best_score)
+print(f"Best solution found:f({best[0]})={best[1]}")
