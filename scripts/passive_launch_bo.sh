e13fd681f1420e7c9f45530547fc87c1abcbcffe
Kiwy3 <nathan.davouse@gmail.com>
2024-10-22 11:15:04 +0200

    file to launch a passive reservation on chuc

---
+++ scripts/passive_launch_bo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#OAR -q testing
+#OAR -p chuc
+#OAR -l walltime=5
+#OAR -O OAR_BO_%jobid%.out
+#OAR -E OAR_BO_%jobid%.err
+
+
+hostname
+date
+
+
+cd ft_poc
+source /home/ndavouse/ft_poc/.venv/bin/activate
+python ./bo.py
