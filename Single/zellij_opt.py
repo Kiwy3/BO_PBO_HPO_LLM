aba2e9ae38c407903268b409416dc24d9f51982b
Kiwy3 <nathan.davouse@gmail.com>
2024-10-24 09:41:27 +0200

    add print to look at variables

--- Single/zellij_opt.py
+++ Single/zellij_opt.py
@@ -12,10 +12,9 @@ from bb_llm import BB_eval
 def loss_function(x):
     HP = {
         "learning_rate": math.exp(x[0]),
-        "lora_rank": x[1],
+        "lora_rank": round(x[1]),
         "fast_run" : True
     }
-    print(dir(x))
     return BB_eval(HP)
 
 loss = Loss()(loss_function)
@@ -55,6 +54,8 @@ def Direct_al(
 
   return sp
 
-sp = Direct_al(values, loss, 1000)
+sp = Direct_al(values, loss, 40)
 best = (sp.loss.best_point, sp.loss.best_score)
 print(f"Best solution found:f({best[0]})={best[1]}")
+print("solutions",sp.loss.all_solutions)
+print("scores",sp.loss.all_scores)
