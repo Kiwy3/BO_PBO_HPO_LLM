9e96a017be5b599861123fbf48b742c9e3fe7b1d
Kiwy3 <nathan.davouse@gmail.com>
2024-10-29 12:43:31 +0100

    testing merging dataset

--- Single/data.py
+++ Single/data.py
@@ -1,154 +1,65 @@
+import os
+import json
 import torch
-from lightning import LightningDataModule
+import litgpt
+from litgpt.data import SFTDataset
+from torch.utils.data import DataLoader, random_split
+import numpy as np
+
+import json
 from dataclasses import dataclass, field
-from pathlib import Path
 from typing import Optional, Union
-
-from datasets import load_dataset, concatenate_datasets
-from torch.utils.data import DataLoader
-
+from pathlib import Path
+from lightning import LightningDataModule
 from litgpt import Tokenizer
-from litgpt.data import  SFTDataset
+from litgpt.data import Alpaca2k, SFTDataset
 from litgpt.prompts import PromptStyle
+from datasets import load_dataset, concatenate_datasets
+from torch.utils.data.distributed import DistributedSampler
 
-@dataclass
-class LLMDataModule(LightningDataModule):
-    mask_prompt: bool = False
-    val_split_fraction: float = 0.05
-    prompt_style: Union[str, PromptStyle] = "alpaca"
-    ignore_index: int = -100
-    seed: int = 42
-    num_workers: int = 4
-    download_dir: Path = Path("./data/alpaca2k")
-    repo_id: str = field(repr=False, default="mhenrichsen/alpaca_2k_test")
-    file_name: str = field(repr=False, default="alpaca2k_data_cleaned_archive.json")
-
-    tokenizer: Optional[Tokenizer] = field(default=None, init=False, repr=False)
-    batch_size: int = field(default=1, init=False, repr=False)
-    max_seq_length: int = field(default=-1, init=False, repr=False)
-    train_dataset: Optional[SFTDataset] = field(default=None, init=False, repr=False)
-    val_dataset: Optional[SFTDataset] = field(default=None, init=False, repr=False)
-
-    def __post_init__(self) -> None:
-        """
-        Initialize the data module.
-
-        This method is called after the data module has been initialized. It sets the
-        prompt_style attribute to an instance of PromptStyle if it is given as a string.
-        """
-        super().__init__()
-        if isinstance(self.prompt_style, str):
-            self.prompt_style = PromptStyle.from_name(self.prompt_style)
-
-    def connect(self, tokenizer: Optional[Tokenizer] = None, batch_size: int = 1, max_seq_length: Optional[int] = None) -> None:
-        """
-        Configure the data module with the specified tokenizer, batch size, and maximum sequence length.
-
-        Args:
-        - tokenizer (Optional[Tokenizer]): The tokenizer to be used for processing text. If not provided, defaults to None.
-        - batch_size (int): The number of samples per batch for training and validation. Defaults to 1.
-        - max_seq_length (Optional[int]): The maximum sequence length for tokenized inputs. If None, defaults to -1.
-
-        Returns:
-        - None
-        """
-        self.tokenizer = tokenizer
+class LLMData:
+    def __init__(self, repo_id, val_split_fraction=0.05, batch_size=1, max_seq_length=512, num_workers=4):
+        self.repo_id = repo_id
+        self.val_split_fraction = val_split_fraction
         self.batch_size = batch_size
-        self.max_seq_length = -1 if max_seq_length is None else max_seq_length
+        self.max_seq_length = max_seq_length
+        self.num_workers = num_workers
+        self.tokenizer = litgpt.Tokenizer(f"checkpoints/TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-    def prepare_data(self) -> None:
-        """
-        Download the dataset from Hugging Face.
+    def prepare_data(self):
+        datasets = []
+        for repo in self.repo_id:
+            datasets.append(load_dataset(repo)["train"])
+        dataset = concatenate_datasets(datasets)
+        train_val_split = dataset.train_test_split(test_size=self.val_split_fraction)
+        train_data = train_val_split["train"]
+        val_data = train_val_split["test"]
 
-        This method downloads the dataset from Hugging Face and caches it in the specified directory.
-
-        Args:
-        - None
-
-        Returns:
-        - None
-        """
-        load_dataset(self.repo_id, cache_dir=self.download_dir)
-
-    def setup(self, stage: str = None) -> None:
-        """
-        Prepare the data module for training and validation.
-
-        This method loads the dataset from Hugging Face, splits it into training and
-        validation sets, and creates SFTDataset instances for training and
-        validation.
-
-        Args:
-        - stage (str): The stage for which the data module is being prepared. If not
-          provided, defaults to None.
-
-        Returns:
-        - None
-        """
-        # Load the dataset
-        dataset = load_dataset(self.repo_id, cache_dir=self.download_dir)
-
-        # Split the dataset into training and validation sets
-        train_validation_split = dataset["train"].train_test_split(test_size=self.val_split_fraction, seed=self.seed)
-        train_data = train_validation_split["train"]
-        val_data = train_validation_split["test"]
-
-        # Create SFTDataset instances for training and validation
         self.train_dataset = SFTDataset(
             data=train_data,
             tokenizer=self.tokenizer,
-            prompt_style=self.prompt_style,
             max_seq_length=self.max_seq_length,
-            mask_prompt=self.mask_prompt,
-            ignore_index=self.ignore_index,
+            prompt_style="alpaca"
         )
         self.val_dataset = SFTDataset(
             data=val_data,
             tokenizer=self.tokenizer,
-            prompt_style=self.prompt_style,
             max_seq_length=self.max_seq_length,
-            mask_prompt=self.mask_prompt,
-            ignore_index=self.ignore_index,
+            prompt_style="alpaca"
         )
 
-    def train_dataloader(self) -> DataLoader:
-        """
-        Create a DataLoader for the training dataset.
+    def get_dataloaders(self, world_size, rank):
+        train_sampler = DistributedSampler(self.train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+        val_sampler = DistributedSampler(self.val_dataset, num_replicas=world_size, rank=rank, shuffle=False)
 
-        This method creates a DataLoader instance with the training dataset and
-        returns it.
+        train_loader = DataLoader(self.train_dataset, batch_size=self.batch_size, sampler=train_sampler, num_workers=self.num_workers)
+        val_loader = DataLoader(self.val_dataset, batch_size=self.batch_size, sampler=val_sampler, num_workers=self.num_workers)
+        return train_loader, val_loader
+    
+data = LLMData(repo_id=["mhenrichsen/alpaca_2k_test", "databricks/databricks-dolly-15k"], val_split_fraction=0.2)
+data.prepare_data()
 
-        Returns:
-        - DataLoader: The DataLoader instance for the training dataset.
-        """
-        out = DataLoader(
-            self.train_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=self.num_workers,
-            generator=torch.Generator().manual_seed(self.seed)
-        )
-        return out
+train_loader, val_loader = data.get_dataloaders(world_size=1, rank=0)
 
-    def val_dataloader(self) -> DataLoader:
-        """
-        Create a DataLoader for the validation dataset.
-
-        This method creates a DataLoader instance with the validation dataset
-        and returns it.
-
-        Returns:
-        - DataLoader: The DataLoader instance for the validation dataset.
-        """
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers,
-        )
-
-
-data = LLMDataModule()
-print(data)
-print(dir(data))
-print("state_dict",data.state_dict())
+for line in train_loader:
+    print(line)
