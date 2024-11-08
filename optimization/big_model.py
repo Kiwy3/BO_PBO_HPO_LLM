d8e9e2dbb9551f6be69ba50b9106e209a336d636
Kiwy3 <nathan.davouse@gmail.com>
2024-11-08 09:10:16 +0100

    minor changes on a test file

--- optimization/big_model.py
+++ optimization/big_model.py
@@ -2,6 +2,7 @@ from model_evaluation import evaluate
 
 if __name__ == "__main__":
     HP = {"fast_run" : True,
-          "model_id" : "meta-llama/Meta-Llama-3.1-8B",
-          "nb_device" : 1}
+          #"model_id" : "meta-llama/Meta-Llama-3.1-8B",
+          "model_id" : "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+          "nb_device" : 2}
     evaluate(HP)
