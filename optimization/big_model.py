eb5ab45fc16ae0948a9b9e6af591cd721d72a3e8
Kiwy3 <nathan.davouse@gmail.com>
2024-11-15 09:39:22 +0100

    clean optimization main dir

--- optimization/big_model.py
+++
@@ -1,8 +0,0 @@
-from model_evaluation import evaluate
-
-if __name__ == "__main__":
-    HP = {"fast_run" : True,
-          #"model_id" : "meta-llama/Meta-Llama-3.1-8B",
-          "model_id" : "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-          "nb_device" : 2}
-    evaluate(HP)
