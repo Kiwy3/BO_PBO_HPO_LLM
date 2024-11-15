c17d4ff1d18c1be52790a73474cf8b825fc03ece
Kiwy3 <nathan.davouse@gmail.com>
2024-11-15 09:38:51 +0100

    remove utilities to put all in utils in model_eval

--- optimization/model_evaluation/utils.py
+++ optimization/model_evaluation/utils.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 def quantize_plug(quantize = "bnb.nf4", precision="16-true"):
     import torch
@@ -31,3 +32,14 @@ def load_config():
     with open("optimization/config.json") as f:
         config = json.load(f)
     return config["hyperparameters"], config["models"], config["experiment"]
+
+def convert(x,i, hyperparameters):
+    key = list(hyperparameters.keys())[i]
+    type = hyperparameters[key]["type"]
+    if type == "int":
+        return int(x[i])
+    elif type == "exp":
+        return math.exp(x[i])
+    elif type == "float":
+        return float(x[i])
+    
