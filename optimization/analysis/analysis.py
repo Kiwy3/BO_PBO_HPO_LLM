48939d4ab17b94a64f900179513ad5809cb94fe5
Kiwy3 <nathan.davouse@gmail.com>
2024-11-15 09:27:33 +0100

    analysis for exp2

--- optimization/analysis/analysis.py
+++ optimization/analysis/analysis.py
@@ -10,26 +10,29 @@ print(Y)
 hp_names = X.columns.to_list()
 
 # Create a (3,2) grid of subplots
-fig, axes = plt.subplots(2, 3, figsize=(12, 9))
+fig, axes = plt.subplots(2, 3, figsize=(16, 10))
 
 # Iterate over the hyperparameter names and plot each scatter plot in a separate subplot
 for i, name in enumerate(hp_names):
     row = i // 3
     col = i % 3
     axes[row, col].scatter(X[name], Y)
-    #axes[row, col].set_title(name)
+    axes[row, col].set_title(name)
     axes[row, col].set_xlabel(name)
     axes[row, col].set_ylabel("MMLU")
+plt.savefig("exp2-bo_individual.png")
 plt.show()
 
 # Create a new subplot for the evolution of Y by iterations
 fig2, ax2 = plt.subplots(figsize=(8, 6))
-ax2.plot(Y)
+ax2.scatter(range(len(Y)),(Y))
 ax2.set_title("Evolution of MMLU by Iterations")
 ax2.set_xlabel("Iteration")
 ax2.set_ylabel("MMLU")
 
 # Show the plots
 plt.tight_layout()
-plt.savefig("bo.png")
+plt.savefig("exp2-bo_mmlu.png")
 plt.show()
+
+print(Y.max())
