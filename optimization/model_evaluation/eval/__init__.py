739c2f89b44a97536c0b5ee2de2a92df911f9953
Kiwy3 <nathan.davouse@gmail.com>
2024-11-18 10:08:40 +0100

    remove evaluate to add it to modelEvaluator

--- optimization/model_evaluation/eval/__init__.py
+++ optimization/model_evaluation/eval/__init__.py
@@ -1,20 +1,6 @@
-__all__ = ["evaluate"]
+__all__ = ["evaluate",
+           "task_evaluate"]
 
 from model_evaluation.eval.hf_eval import convert_and_evaluate as task_evaluate
 from model_evaluation.utils import load_hyperparameters, add_results, load_config
 
-
-def evaluate():
-    lora_path = "checkpoints/lora"
-    _, _, experiment = load_config()
-    tasks = experiment["tasks"]
-    eval_limit = experiment["eval_limit"]
-    results = task_evaluate(lora_path,
-                        tasks=tasks[0] if len(tasks) == 1 else tasks,
-                        limit=eval_limit,
-                        force_conversion=True,
-                        out_dir="eval/")
-    res = {}
-    for task in tasks:
-        res[task] =  results[task]["acc,none"]
-    return res
