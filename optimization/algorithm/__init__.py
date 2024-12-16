f478d0aa7e3fbac38c13a46a0430fe73ba6ae695
Kiwy3 <nathan.davouse@gmail.com>
2024-12-16 09:38:17 +0100

    add bamsoo and SOO to algorithm

--- optimization/algorithm/__init__.py
+++ optimization/algorithm/__init__.py
@@ -1,3 +1,8 @@
 from algorithm.bo_class import BO_HPO
+from algorithm.soo import SOO, array
+from algorithm.bamsoo import BaMSOO
 
-__all__ = ["BO_HPO"]
+__all__ = ["BO_HPO",
+           "SOO",
+           "array",
+           "BaMSOO"]
