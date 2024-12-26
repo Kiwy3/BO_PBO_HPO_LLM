187a9dbd82625f8179c5c809aeb05b7135bf00de
Kiwy3 <nathan.davouse@gmail.com>
2024-12-26 15:41:54 +0100

    eval on three sections to ensure fonctionnal run

--- main.py
+++ main.py
@@ -5,23 +5,41 @@ from optimization_v2.toolbox import ModelEval, SearchSpace, Solution
 
 space = SearchSpace(mode="base",
     savefile="savefile.json",)
-x = space.center
+spaces = space.section(3)
 
-""" evaluator = ModelEval(
+evaluator = ModelEval(
     search_space = space,
     dev_run="real"
 )
 
-out = evaluator.train_and_evaluate(x)
+""" results = []
+for sp in spaces : 
+    x = sp.get_center()
+    out = evaluator.train_and_evaluate(x)
+    results.append(out)
+    print(out)
 
-print("out : ",out)
+
+print("final results : ", results)
  """
+results = []
 
+x1 = space.get_solution(
+    x=[32,1,0,0]
+)
+y1 = evaluator.train_and_evaluate(x1)
+results.append(y1)
 
-spaces = space.section(3)
+x2 = space.get_solution(
+    x=[64,1,0,1]
+)
+y2 = evaluator.train_and_evaluate(x2)
+results.append(y2)
 
-for i,sp in enumerate(spaces): 
-    print("space number : ",i)
-    print("\t center : ", sp.get_center().get_values())
-    for value in sp.variables.values():
-        print("\t ",value.get_dict())
+x3 = space.get_solution(
+    x=[8,1,0,2]
+)
+y3 = evaluator.train_and_evaluate(x3)
+results.append(y3)
+
+print("final results : ", results)
