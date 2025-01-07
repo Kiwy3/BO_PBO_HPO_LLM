ea096838e22c4914347169aea4d9ccb83cf568d2
Kiwy3 <nathan.davouse@gmail.com>
2025-01-07 11:17:02 +0100

    clean repo (remove old optimization and bamsoo)

--- optimization/model_evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-from model_evaluation.train_model import training
-from model_evaluation.model_eval import ModelEvaluator
-
-__all__ = ["training",
-           "ModelEvaluator"]
--- optimization/run.py
+++
@@ -1,9 +0,0 @@
-from algorithm import BO_HPO
-
-if __name__ == "__main__":
-
-    g=10
-    bo = BO_HPO()
-    bo.init()
-    print(bo.X,"\n", bo.Y, bo.bounds)
-    bo.run(n=20)
