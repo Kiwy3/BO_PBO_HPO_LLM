8da9ebfb7463b94e6d1eb947d291cdd12bfc545d
Kiwy3 <nathan.davouse@gmail.com>
2025-01-09 12:04:16 +0100

    add variables over time

--- results/analysis.py
+++ results/analysis.py
@@ -5,7 +5,7 @@ import json
 
 # Manage file name depending on interactive env or not
 
-exp_name = "exp11"
+exp_name = "exp12"
 file_name = exp_name + ".json"
 import __main__
 if not hasattr(__main__,"__file__"):
@@ -82,12 +82,28 @@ conv_values.rename(
 """--------------------------Plots ------------------------"""
 # Score over time plots
 plt.title(f"{exp_algo[exp_name]} : Score over time")
-plt.scatter(score.index, score["acc_norm"])
+plt.scatter(score.index, score["acc_norm"], marker = "x")
 plt.xlabel("iterations")
 plt.ylabel("Hellaswag acc_norm")
 if exp_name == "exp12" : plt.axvline(10, color='r', linestyle='dashed', linewidth=1, label="sampling")
 plt.legend()
 plt.savefig(f"plots/{exp_name}_score_over_time.png")
+plt.show()
+
+
+# Variables over time
+hp_short = ["rank", "alpha", "dropout", "lr", "decay"]
+fig, ax = plt.subplots(len(hp), 1, sharex=True)
+fig.figsize=(10, 25)
+for i in range(len(hp)):
+    ax[i].scatter(base_values.index, conv_values[hp[i]], marker = "x")
+    ax[i].set_ylabel(hp_short[i])
+    if exp_name == "exp12" : ax[i].axvline(10, color='r', linestyle='dashed', linewidth=1, label="sampling")
+ax[-1].set_xlabel('iterations')
+fig.suptitle(f"{exp_algo[exp_name]} : hyperparameters over iterations")
+plt.savefig(f"plots/{exp_name}_variables_over_time.png")
+plt.show()
+
 
 
 
