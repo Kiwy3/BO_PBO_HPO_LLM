a05e0aeb824ec5a0097d8ac96b42d93f91e847ca
Kiwy3 <nathan.davouse@gmail.com>
2025-01-22 13:57:18 +0100

    bo final resutls

--- computation_experiments/bo_analysis.py
+++ computation_experiments/bo_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
 exp_name = "bo_bis"
 file_name = "results/" + exp_name + ".json"
@@ -28,7 +29,35 @@ hp_names = ["lora_rank", "lora_alpha", "lora_dropout", "learning_rate", "weight_
 Solution = data["solution"].apply(lambda x : x["base_value"])
 Solution = pd.DataFrame(Solution.to_list(),columns=hp_names)
 
-plt.figure(figsize=(10, 6))
-sns.scatterplot(score)
-plt.title("BO : score over iterations")
-plt.savefig(f"plots/bo_score_over_time.png")
+
+"""--------------------- Score over iterations figure ---------------------- """
+plt.figure(figsize=(6, 4))
+sns.scatterplot(x=score.index+1,y=score["hellaswag"],label = "evaluation")
+sns.scatterplot(x=score[:10].index+1,y=score[:10]["hellaswag"],label = "sampled points")
+
+max_index = np.argmax(score["hellaswag"])
+max_score = score["hellaswag"][max_index]
+plt.scatter(max_index+1,max_score,color="red",label="best point (Hellaswag)")
+
+plt.xlim(0,51)
+plt.ylim(0.2,0.5)
+plt.xlabel("iterations")
+plt.ylabel("Hellaswag accuracy")
+plt.vlines(10,0.2,0.5,colors="red",linestyles="dashed",label="sampling phase")
+plt.legend(loc="lower right")
+#plt.title("BO : score over iterations")
+plt.savefig(f"plots/bo/score_evolution.png")
+
+
+"""--------------------- Variables over time ---------------------- """
+fig, ax = plt.subplots(5,1,sharex=True,figsize=(4, 6))
+
+for i in range(len(hp_names)):
+    ax[i].scatter(score.index+1,Solution[hp_names[i]], marker = "x")
+    ax[i].scatter(score[:10].index+1,Solution[:10][hp_names[i]], marker = "x")
+    ax[i].scatter(max_index+1,Solution[hp_names[i]][max_index],color="red",marker="x",label="best point (Hellaswag)")
+    ax[i].set_ylabel(hp_names[i])
+ax[-1].set_xlabel('iterations')
+fig.tight_layout()
+plt.savefig(f"plots/bo/variables_evolution.png")
+plt.show()
