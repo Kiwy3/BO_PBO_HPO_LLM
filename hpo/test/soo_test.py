210eeb0bd14f9a2a3c27299c19b0421187d9bfbc
Kiwy3 <nathan.davouse@gmail.com>
2025-01-23 12:14:38 +0100

    functionnal test for soo

---
+++ hpo/test/soo_test.py
@@ -0,0 +1,21 @@
+from hpo.core.SearchSpace import SearchSpace, Solution
+from algorithm.soo import SOO
+
+def himmelblau(solution : Solution) :
+        x = solution.get_values()
+        y = (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
+        solution.add_score(y)
+        return y
+
+space_dict = {
+    "x1" : {"min" : -5,"max" : 5, "type":"float"},
+    "x2" : {"min" : -5,"max" : 5,"type":"float"}
+}
+space = SearchSpace(
+    space_dict)
+algo = SOO(search_space=space,
+            K=3,
+            maximizer=False,
+            objective_function=himmelblau)
+algo.run(budget=50)
+algo.print()
